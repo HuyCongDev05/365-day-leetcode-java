d240246027a8329d6ed75105f8c9f7cc26ef48f2
HuyCongDev05 <congtuoitre1007@gmail.com>
2025-09-13 20:52:33 +0700

    day 9

--- day_8/src/Main.java
+++ day_8/src/Main.java
@@ -12,15 +12,20 @@ public class Main {
     public static void main(String[] args) {
         class Solution {
             public int[] productExceptSelf(int[] nums) {
-                int[] res = new int[nums.length];
-                int core = 1;
-                for ( int i = 0; i < nums.length; i++ ) {
-                    for ( int j = 0; j < nums.length; j++ ) {
-                        if (i != j) core = core * nums[j];
-                    }
-                    res[i] = core;
-                    core = 1;
+                int n = nums.length;
+                int[] res = new int[n];
+
+                res[0] = 1;
+                for (int i = 1; i < n; i++) {
+                    res[i] = res[i - 1] * nums[i - 1];
                 }
+
+                int right = 1;
+                for (int i = n - 1; i >= 0; i--) {
+                    res[i] *= right;
+                    right *= nums[i];
+                }
+
                 return res;
             }
         }
