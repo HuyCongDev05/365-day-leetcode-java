058a29d577a326200e0f3ff858de1c15c48dd1ea
HuyCongDev05 <congtuoitre1007@gmail.com>
2025-10-24 23:32:46 +0700

    day 17

---
+++ 365_day_leetcode_java/src/day_17.java
@@ -0,0 +1,20 @@
+public class day_17 {
+    public static void main(String[] args) {
+        class Solution {
+            public double findMaxAverage(int[] nums, int k) {
+                double max_avg = -Double.MAX_VALUE, total = 0;
+                for (int i = 0; i < k; i++) {
+                    total += nums[i];
+                    max_avg = total/k;
+                }
+                for (int i = k; i < nums.length; i++) {
+                    total = total - nums[i -k] + nums[i];
+                    if (max_avg < total/k) max_avg = total/k;
+                }
+                return max_avg;
+            }
+        }
+        Solution solution = new Solution();
+        System.out.println(solution.findMaxAverage(new int[]{-1},1));
+    }
+}
